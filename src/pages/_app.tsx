6e2b65800f6b82b505ba19a227f525cd2ed6305f
msy7822-ux <msy780022@gmail.com>
2023-05-05 16:13:47 +0900

    feat: add Google OAuth provider and customize session to include access token fix: import getSession from next-auth/react in _app.tsx feat: add login page with sign up button feat: customize session type to include access token in next-auth.d.ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, getSession } from "next-auth/react";
 
 export default function App({
   Component,
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,45 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-export const authOptions = {
-  // Configure one or more authentication providers
+import GoogleProvider from "next-auth/providers/google";
+import { Session } from "next-auth";
+
+interface CustomSession extends Session {
+  accessToken: string;
+}
+
+export default NextAuth({
   providers: [
-    // Google providerに修正する
-    GithubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
-    // ...add more providers here
   ],
-};
-export default NextAuth(authOptions);
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // Send properties to the client, like an access_token from a provider.
+      const customeSession = session as CustomSession;
+      customeSession.accessToken = (token.accessToken as string) ?? "";
+      return session;
+    },
+    async signIn() {
+      console.log("サインイン");
+      return true;
+    },
+    async redirect() {
+      return "/";
+    },
+  },
+});
