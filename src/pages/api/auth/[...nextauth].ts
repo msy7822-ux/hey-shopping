737885a935dbadee122adaf9f9aefe20689cd195
msy7822-ux <msy780022@gmail.com>
2023-05-08 10:57:44 +0900

    sesson永続化！🚀

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,16 @@
-import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { Session } from "next-auth";
+import NextAuth, { Session, NextAuthOptions } from "next-auth";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 interface CustomSession extends Session {
   accessToken: string;
 }
 
-export default NextAuth({
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, authOptions);
+}
+
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
@@ -22,17 +26,16 @@ export default NextAuth({
   ],
   callbacks: {
     async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
       if (account) {
         token.accessToken = account.access_token;
       }
       return token;
     },
     async session({ session, token }) {
-      // Send properties to the client, like an access_token from a provider.
       const customeSession = session as CustomSession;
       customeSession.accessToken = (token.accessToken as string) ?? "";
-      return session;
+
+      return customeSession;
     },
     async signIn() {
       console.log("サインイン");
@@ -42,4 +45,9 @@ export default NextAuth({
       return "/";
     },
   },
-});
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+    updateAge: 24 * 60 * 60,
+  },
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { useSession } from "next-auth/react";
+import { useSession, getSession } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const session = useSession();
-  console.log("client", session.data);
-
+  const { data: session } = useSession();
+  console.log("index", session);
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
